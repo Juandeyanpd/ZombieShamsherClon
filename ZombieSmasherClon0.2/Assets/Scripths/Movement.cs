71b2213372824818338d1399291bd19997c2d724
unknown <SENA@MEDAPRCCSFSD350.FormacionSena.RED>
2022-06-15 14:51:46 -0500

    Proyecto Base

---
+++ ZombieSmasherClon0.2/Assets/Scripths/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Matar))]
+public class Movement : MonoBehaviour
+{
+    public float velocidadDeMovimiento;
+    private int numeroAleatorio;
+    private SpriteRenderer SpriteRenderer;
+    public Transform puntoDeMovimiento;
+
+    private void Start()
+    {
+        //SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, puntoDeMovimiento.position, velocidadDeMovimiento * Time.deltaTime);
+    }
+}
